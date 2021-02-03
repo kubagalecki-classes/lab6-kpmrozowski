48fd35535e28f89267a36dfd8e37ebfac4162e58
kpmrozowski <kpmrozowski@gmail.com>
2021-02-03 15:23:37 +0000

    cwI->std::adjacent_find

--- lab6.cpp
+++ lab6.cpp
@@ -1,53 +1,55 @@
 #include <algorithm>
 #include <iostream>
 #include <random>
+#include <string>
 #include <type_traits>
 #include <vector>
 
 template < typename T >
-std::vector< T > makeRandomVector(size_t size, T minValue, T maxValue)
+std::vector< T > make_random_vector(size_t size, T min_value, T max_value)
 {
     static_assert(std::is_arithmetic_v< T >, "T must be an arithmetic type");
 
     std::mt19937     prng{std::random_device{}()};
-    std::vector< T > retV;
-    retV.reserve(size);
-    auto bi = std::back_inserter(retV);
+    std::vector< T > ret_v;
+    ret_v.reserve(size);
+    auto bi = std::back_inserter(ret_v);
 
     using dist_t = std::conditional_t< std::is_integral_v< T >,
                                        std::uniform_int_distribution< T >,
                                        std::uniform_real_distribution< T > >;
 
-    dist_t dist{minValue, maxValue};
+    dist_t dist{min_value, max_value};
     std::generate_n(bi, size, [&]() { return dist(prng); });
 
-    return retV;
+    return ret_v;
 }
 
-void printVector(std::vector< int > wektor)
+/* zad I */
+
+bool areBoolsEqual(char a, char b)
 {
-    for (const int i : wektor)
-        std::cout << i << " ";
-    std::cout << std::endl;
+    return (a == b);
 }
 
-bool isGreater(int i, int j)
+bool areTwoCharsInaRow()
 {
-    return (i > j);
+    std::string s;
+    std::cout << "Podaj zmienna typu string" << std::endl;
+    std::cin >> s;
+    std::string::iterator it = std::adjacent_find(s.begin(), s.end());
+    if (it == s.end())
+        return false;
+    else {
+        std::cout << "Znaleziono powtorzenie litery " << (*it) << std::endl;
+        return true;
+    }
 }
 
 int main()
 {
-    std::vector< int > wektor = makeRandomVector< int >(10, 0, 10);
-    printVector(wektor);
-    std::sort(wektor.begin(), wektor.end(), isGreater);
-    printVector(wektor);
-    std::cout << "Liczba wystapien elementu 7:";
-    std::cout << std::count(wektor.begin(), wektor.end(), 7) << std::endl;
-    std::cout << "Podaj liczbe:" << std::endl;
-    int a;
-    std::cin >> a;
-    std::cout << "Liczba wystapien elementow wiekszych od podanej liczby:" << std::endl;
-    std::cout << std::count_if(wektor.begin(), wektor.end(), [&](int i) { return (i > a); })
-              << std::endl;
+    if (areTwoCharsInaRow())
+        std::cout << "Para znaleziona" << std::endl;
+    else
+        std::cout << "Para nie zostala znaleziona" << std::endl;
 }
